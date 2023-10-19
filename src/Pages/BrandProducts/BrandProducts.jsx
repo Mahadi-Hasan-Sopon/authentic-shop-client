313fd594a2f048bba8c05b2f59e959af64604d68
Mahadi Hasan <mahadih137@gmail.com>
2023-10-19 16:21:20 +0600

    brandProducts page updated

--- src/Pages/BrandProducts/BrandProducts.jsx
+++ src/Pages/BrandProducts/BrandProducts.jsx
@@ -1,21 +1,41 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import ProductCard from "../../Components/ProductCard/ProductCard";
+import { useContext } from "react";
+import { AuthContext } from "../../Contexts/AuthContextProvider";
+import LoadingSpinner from "../../utils/LoadingSpinner/LoadingSpinner";
 
 function BrandProducts() {
+  const { isLoading } = useContext(AuthContext);
   const loadedBrandProducts = useLoaderData();
   const params = useParams();
 
   console.log(loadedBrandProducts);
   return (
-    <div className="max-w-7xl xl:mx-auto px-6">
-      <h2 className="text-3xl font-bold text-slate-100 mb-6">
-        {params.brandName} Brand Products
-      </h2>
-      <div className="products grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-        {loadedBrandProducts?.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
-      </div>
+    <div className="max-w-7xl lg:mx-auto px-6">
+      {isLoading && <LoadingSpinner />}
+      {loadedBrandProducts.length < 1 ? (
+        <div className="flex justify-center items-center flex-col my-20">
+          <h2 className="text-5xl font-bold text-white">
+            Sorry, No Product Found!
+          </h2>
+          <img
+            src="https://i.ibb.co/09NnrDb/product-is-empty-8044872-6430781.png"
+            alt=""
+          />
+        </div>
+      ) : (
+        <div className=" flex flex-col mx-auto justify-center items-center my-20">
+          <h2 className="text-3xl font-bold text-slate-100 mb-6">
+            {params.brandName} Brand Products
+          </h2>
+
+          <div className="products grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+            {loadedBrandProducts?.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/utils/Rating/Rating.jsx
+++ src/utils/Rating/Rating.jsx
@@ -3,43 +3,23 @@ import { FaStar } from "react-icons/fa";
 
 function Rating({ rating }) {
   const { rate, count } = rating;
-  //   const normalizeRating = Math.min(5, Math.max(0, rate));
-  //   const stars = Array.from({ length: 5 }, (_, index) => (
-  //     <FaStar
-  //       key={index}
-  //       color={index < normalizeRating ? "gold" : "gray"}
-  //       size={22}
-  //     />
-  //   ));
-  const maxStars = 5;
-  const starPercentage = (rate / maxStars) * 100;
-  const filledStars = Math.floor(starPercentage / 20); // Each star represents 20%.
-  const lastStarFill = (starPercentage % 20) / 20; // Calculate the fill percentage for the last star.
 
-  const stars = [];
+  const fillPercentage = (rate / 5) * 100;
 
-  for (let i = 0; i < filledStars; i++) {
-    stars.push(<FaStar key={i} color="gold" size={24} />);
-  }
+  // Create an array of 5 stars
+  const stars = Array.from({ length: 5 }, (_, index) => (
+    <span
+      key={index}
+      className={
+        index < fillPercentage / 20 ? "text-yellow-400" : "text-gray-200"
+      }
+    >
+      <FaStar />
+    </span>
+  ));
 
-  if (lastStarFill > 0) {
-    stars.push(
-      <FaStar
-        key={filledStars}
-        color="gold"
-        size={24}
-        style={{ clipPath: `inset(0 ${100 - lastStarFill * 100}% 0 0 )` }}
-      />
-    );
-  }
-
-  const emptyStars = maxStars - stars.length;
-
-  for (let i = 0; i < emptyStars; i++) {
-    stars.push(<FaStar key={`empty-${i}`} color="gray" size={24} />);
-  }
   return (
-    <div className="star-rating flex">
+    <div className="star-rating flex items-center">
       {stars} <span className="ms-2">{count} review</span>
     </div>
   );
